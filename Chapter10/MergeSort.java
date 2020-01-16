e30d56a53f84bcd96088ceee1756fc07b56352f2
Hong-Yi Wang <wanghy2007@gmail.com>
2020-01-15 16:20:30 -0800

    Add MergeSort

---
+++ Chapter10/MergeSort.java
@@ -0,0 +1,56 @@
+package Chapter10;
+
+import java.util.Arrays;
+
+/**
+ * MergeSort
+ */
+public class MergeSort {
+
+    static void mergesort(int[] array) {
+        int[] helper = new int[array.length];
+        mergesort(array, helper, 0, array.length-1);
+    }
+
+    static void mergesort(int[] array, int[] helper, int low, int high) {
+        if (low < high) {
+            int middle = (low + high) / 2;
+            mergesort(array, helper, low, middle);
+            mergesort(array, helper, middle+1, high);
+            merge(array, helper, low, middle, high);
+        }
+    }
+
+    static void merge(int[] array, int[] helper, int low, int middle, int high) {
+        for (int i = low; i <= high; i++) {
+            helper[i] = array[i];
+        }
+
+        int helperLeft = low;
+        int helperRight = middle + 1;
+        int current = low;
+
+        while (helperLeft <= middle && helperRight <= high) {
+            if (helper[helperLeft] <= helper[helperRight]) {
+                array[current] = helper[helperLeft];
+                helperLeft++;
+            } else {
+                array[current] = helper[helperRight];
+                helperRight++;
+            }
+            current++;
+        }
+
+        while (helperLeft <= middle) {
+            array[current] = helper[helperLeft];
+            helperLeft++;
+            current++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{1, 4, 5, 2, 8, 9};
+        mergesort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
