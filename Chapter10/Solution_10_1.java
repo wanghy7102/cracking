36a8d3700bb93551d2367089754d8ba67ff98172
wanghy7102 <wanghy7102@gmail.com>
2020-01-15 19:37:44 -0800

    Revise Solution_10_1

--- Chapter10/Solution_10_1.java
+++ Chapter10/Solution_10_1.java
@@ -7,46 +7,33 @@ import java.util.Arrays;
  */
 public class Solution_10_1 {
 
-    static void sortedMerge(int[] a, int[] b, int sizeA, int sizeB) {
-        int[] h = new int[a.length];
-        for (int i = 0; i < sizeA; i++) {
-            h[i] = a[i];
-        }
-        for (int i = 0; i < sizeB; i++) {
-            h[i+sizeA] = b[i];
-        }
+    static void merge(int[] a, int[] b, int lastA, int lastB) {
+        int indexA = lastA - 1;
+        int indexB = lastB - 1;
+        int indexMerged = lastB + lastA - 1;
 
-        int helperA = 0;
-        int helperB = sizeA;
-        int current = 0;
-        while (helperA < sizeA && helperB < sizeA+sizeB) {
-            if (h[helperA] <= h[helperB]) {
-                a[current] = h[helperA];
-                helperA++;
+        while (indexA >= 0 && indexB >= 0) {
+            if (a[indexA] > b[indexB]) {
+                a[indexMerged] = a[indexA];
+                indexA--;
             } else {
-                a[current] = h[helperB];
-                helperB++;
+                a[indexMerged] = b[indexB];
+                indexB--;
             }
-            current++;
+            indexMerged--;
         }
 
-        while (helperA < sizeA) {
-            a[current] = h[helperA];
-            helperA++;
-            current++;
-        }
-
-        while (helperB < sizeA+sizeB) {
-            a[current] = h[helperB];
-            helperB++;
-            current++;
+        while (indexB >= 0) {
+            a[indexMerged] = b[indexB];
+            indexB--;
+            indexMerged--;
         }
     }
 
     public static void main(String[] args) {
         int[] a = new int[]{2,4,6,0,0,0,0};
         int[] b = new int[]{1,3,5,7};
-        sortedMerge(a, b, 3, 4);
+        merge(a, b, 3, 4);
         System.out.println(Arrays.toString(a));
     }
 }
